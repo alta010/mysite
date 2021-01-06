4d28720235357c6e7233570ed23cc6f23d4646d4
alta010 <erikburguenosbhs@gmail.com>
2021-01-06 01:54:20 -0800

    update

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    #path('', views.PostList.as_view(), name='home'),
+    #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', views.index)
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,13 @@ from django.views import generic
 
 # Create your views here.
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+#class PostList(generic.ListView):
+#    queryset = Post.objects.filter(status=1).order_by('-created_on')
+#    template_name = 'index.html'
 
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+#class PostDetail(generic.DetailView):
+#    model = Post
+#    template_name = 'post_detail.html'
 
-#def index(request):
-#    return render(request, 'blog/main.html')
+def index(request):
+    return render(request, 'blog/main.html')
