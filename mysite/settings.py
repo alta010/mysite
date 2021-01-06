93723842b6b9a5290d7cd902be6a4780cfc9ffbf
alta010 <erikburguenosbhs@gmail.com>
2021-01-05 23:27:22 -0800

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,8 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static/')
